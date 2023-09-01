ee656cd884b75d394f2f19570889265405e8b756
keeemhs <alenen0227@naver.com>
2023-09-01 15:46:34 +0900

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// index.ejs
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// 404 error
+app.use('*', (req, res) => {
+    res.render('404');
+});
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
