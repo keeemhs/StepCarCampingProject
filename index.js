5efbd5237be1712885441b73ae0326117524b8db
keeemhs <alenen0227@naver.com>
2023-09-01 20:57:48 +0900

    footer ejs

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const PORT = 8000;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('./views/css')); // css폴더 경로 셋팅
 
 // index.ejs
 app.get('/', (req, res) => {
