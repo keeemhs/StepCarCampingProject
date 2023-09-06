d73268aefb0d927d1bab198ab7928ac1f7bf3a09
transfact <transpaction@naver.com>
2023-09-06 20:54:29 +0900

    gallery delete btn

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,20 +1,18 @@
-const express = require("express")
-const galleryController =require("../controller/Cgallery")
-const router = express.Router()
+const express = require('express');
+const galleryController = require('../controller/Cgallery');
+const router = express.Router();
 
 // 리뷰 페이지로
-router.get("/review",galleryController.reviewPage)
-
+router.get('/review', galleryController.reviewPage);
 
 //갤러리 리뷰 페이지로
-router.get("/reviewEdit",galleryController.reviewEdit)
+router.get('/reviewEdit', galleryController.reviewEdit);
 //갤러리 axiosTest
-router.post("/multipleAxios",galleryController.multipleAxios)
+router.post('/multipleAxios', galleryController.multipleAxios);
 
 //review 만들떄 싱글 axios
-router.post("/singleAxios",galleryController.singleAxios)
-
+router.post('/singleAxios', galleryController.singleAxios);
 
 //갤러리 axiosTest
-router.delete("/gallery/del",galleryController.multipleAxios)
+router.delete('/review/del', galleryController.reviewDel);
 module.exports = router;
