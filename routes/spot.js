af4ce495bd4e56a47175bf6667a986d51a4ce106
NavyBlueSky <lylebang9397@gmail.com>
2023-09-11 17:35:22 +0900

    chat edit

--- routes/spot.js
+++ routes/spot.js
@@ -2,11 +2,17 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controller/Cspot')
 
-//GET
-router.get('/', controller.spot)
+// module.exports = router
 
-//POST
-router.post('/location', controller.location)
+module.exports = function (io) {
+    //GET
+    router.get('/', controller.spot)
 
+    //POST
+    router.post('/location', controller.location)
 
-module.exports = router
+    // io.on('connection', (socket) => {
+    //     controller.connection(io, socket);
+    // });
+    return router
+};
